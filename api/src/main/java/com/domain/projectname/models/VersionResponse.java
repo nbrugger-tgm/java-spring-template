58787020e4af4ec93bd3244c4e6d80ae5eda5b7f
Nils Brugger <nilsbrugger.nb.nb.nb@gmail.com>
2021-11-07 07:10:42 +0100

    build: fix build
    fix: wrong caching folder

--- api/src/main/java/com/domain/projectname/models/VersionResponse.java
+++ api/src/main/java/com/domain/projectname/models/VersionResponse.java
@@ -3,15 +3,22 @@ package com.domain.projectname.models;
 import com.vdurmont.semver4j.Semver;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.validation.constraints.Pattern;
 
+import static lombok.AccessLevel.PRIVATE;
+
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class VersionResponse {
 	@Pattern(regexp = "^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+([0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?$")
-	private final String  version;
-	private final boolean isStable;
+	@Setter(PRIVATE)
+	private String  version;
+	@Setter(PRIVATE)
+	private boolean isStable;
 
 	public VersionResponse(Semver version) {
 		this(
