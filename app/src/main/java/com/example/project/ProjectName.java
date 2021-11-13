810c74527868a279a9d37b2070dcdb1828767143
Nils Brugger <nilsbrugger.nb.nb.nb@gmail.com>
2021-11-13 19:00:38 +0100

    docs: reset to base version

--- app/src/main/java/com/example/project/ProjectName.java
+++ app/src/main/java/com/example/project/ProjectName.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectName {
 
-	public static final Semver VERSION = new Semver("0.1.0");
+	public static final Semver VERSION = new Semver("0.0.0");
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectName.class, args);
