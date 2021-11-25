6726e051b88ddbb80a9d6ede411fc3e0bd6375ee
Nils Brugger <nilsbrugger.nb.nb.nb@gmail.com>
2021-11-26 00:48:14 +0100

    docs: add api annotations

--- modules/api/src/main/java/com/domain/projectname/api/controllers/ApiInfoController.java
+++ modules/api/src/main/java/com/domain/projectname/api/controllers/ApiInfoController.java
@@ -1,13 +1,18 @@
 package com.domain.projectname.api.controllers;
 
 import com.domain.projectname.models.VersionDto;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @RequestMapping("api")
 public interface ApiInfoController {
-	@GetMapping("version")
-	VersionDto version();
+
+	@Operation(
+			summary = "Get API version",
+			description = "Get API version",
+			tags = "ApiInfo"
+	)
+	@GetMapping("/version")
+	VersionDto getVersion();
 }
