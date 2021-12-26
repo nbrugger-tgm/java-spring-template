c8cbf85d5bf9df66755405824a70ff45d2d11b82
Nils Brugger <nilsbrugger.nb.nb.nb@gmail.com>
2021-12-26 21:25:10 +0100

    feat: automated deployments

--- modules/app/src/main/java/com/domain/projectname/ProjectName.java
+++ modules/app/src/main/java/com/domain/projectname/ProjectName.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectName {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProjectName.class, args);
 	}
 
